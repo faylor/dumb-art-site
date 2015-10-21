c2cb6368d2ed391cad945c62f74298c4053e23c4
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-10-21 15:29:59 +0100

    fixed routes

--- server.js
+++ server.js
@@ -133,6 +133,7 @@ var SampleApp = function() {
             form.parse(req, function(err, fields, files) {
                               res.writeHead(200, {'content-type': 'text/plain'});
                               res.write('received upload:\n\n');
+                              res.write(self.imagedir);
                               res.end(util.inspect({fields: fields, files: files}));
                             });
 
