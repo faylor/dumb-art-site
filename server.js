681c30f7e254c21ee151b84786283b69559c1069
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-10-21 14:45:13 +0100

    updated pagination

--- server.js
+++ server.js
@@ -134,18 +134,19 @@ var SampleApp = function() {
                             });
 
             form.on('end', function(fields, files) {
-            /* Temporary location of our uploaded file */
-            var temp_path = this.openedFiles[0].path;
-            /* The file name of the uploaded file */
-            var file_name = this.openedFiles[0].name;
-            /* Location where we want to copy the uploaded file */
+                /* Temporary location of our uploaded file */
+                var temp_path = this.openedFiles[0].path;
+                /* The file name of the uploaded file */
+                var file_name = this.openedFiles[0].name;
+                /* Location where we want to copy the uploaded file */
 
-            fs.copy(temp_path, imagedir + file_name, function(err) {
-              if (err) {
-                console.error(err);
-              } else {
-                console.log("success!")
-              }
+                fs.copy(temp_path, imagedir + file_name, function(err) {
+                    if (err) {
+                      console.error(err);
+                    } else {
+                      console.log("success!")
+                    }
+                });
             });
         };
     };
