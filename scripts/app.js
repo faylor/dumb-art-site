6e7340087919ca97b5a43eb3e63468376ff06cd8
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-10-22 16:28:03 +0100

    making more angular

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute'])
+var app = angular.module('app', ['ngRoute','app.contact'])
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
