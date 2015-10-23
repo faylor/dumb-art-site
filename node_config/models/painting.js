aeccfd2627dc54f62e6f21e1e7ff27e92ff8d52c
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-10-23 16:58:43 +0100

    making more angular

---
+++ node_config/models/painting.js
@@ -0,0 +1,117 @@
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+/**
+ * Getters
+ */
+var getTags = function (tags) {
+  return tags.join(',');
+};
+
+/**
+ * Setters
+ */
+var setTags = function (tags) {
+  return tags.split(',');
+};
+/**
+ * Painting Schema
+ */
+
+var PaintingSchema = new Schema({
+  title: {type : String, default : '', trim : true},
+  body: {type : String, default : '', trim : true},
+  size: {type : String, default : '', trim : true},
+  price: {type : Number, default : 0},
+  tags: {type: [], get: getTags, set: setTags},
+  image: {type : String, default : '', trim : true},
+  createdAt  : {type : Date, default : Date.now}
+});
+
+/**
+ * Validations
+ */
+
+PaintingSchema.path('title').required(true, 'Painting title cannot be blank');
+PaintingSchema.path('size').required(true, 'Painting size cannot be blank');
+
+/**
+ * Pre-remove hook
+ */
+
+PaintingSchema.pre('remove', function (next) {
+  var files = this.image;
+
+  // if there are files associated with the item, remove from the cloud too
+
+  next();
+});
+
+/**
+ * Methods
+ */
+
+PaintingSchema.methods = {
+
+  /**
+   * Save Painting and upload image
+   *
+   * @param {Object} images
+   * @param {Function} cb
+   * @api private
+   */
+
+  uploadAndSave: function (images, cb) {
+    if (!images || !images.length) return this.save(cb)
+
+    //upload here
+  }
+}
+
+/**
+ * Statics
+ */
+
+PaintingSchema.statics = {
+
+  /**
+   * Find Painting by id
+   *
+   * @param {ObjectId} id
+   * @param {Function} cb
+   * @api private
+   */
+
+  load: function (id, cb) {
+    this.findOne({ _id : id })
+      .populate('user', 'name email username')
+      .exec(cb);
+  },
+
+  /**
+   * List Paintings
+   *
+   * @param {Object} options
+   * @param {Function} cb
+   * @api private
+   */
+
+  list: function (options, cb) {
+    var criteria = options.criteria || {}
+
+    this.find(criteria)
+      .populate('user', 'name username')
+      .sort({'createdAt': -1}) // sort by date
+      .limit(options.perPage)
+      .skip(options.perPage * options.page)
+      .exec(cb);
+  }
+}
+
+mongoose.model('Painting', PaintingSchema);
---
+++ node_config/routes.js
@@ -0,0 +1,70 @@
+module.exports = function(self) {
+    self.getroutes = { };
+
+    self.getroutes['/'] = function(req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(self.cache_get('index.html') );
+    };
+/*
+    self.getroutes['/gallery'] = function(req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(self.cache_get('gallery.html') );
+    };
+*/
+    self.getroutes['/returnAllPaintings'] = function(req, res){
+        self.db.collection('Paintings').find().toArray(function(err, names) {
+            res.header("Content-Type:","text/json");
+            res.end(JSON.stringify(names));
+        });
+    };
+    self.getroutes['/upload'] = function (req, res){
+      res.writeHead(200, {'Content-Type': 'text/html' });
+      var form = '<form action="/upload" enctype="multipart/form-data" method="post">Add a title: <input name="title" type="text" /><br><br><input multiple="multiple" name="upload" type="file" /><br><br><input type="submit" value="Upload" /></form>';
+      res.end(form);
+    };
+
+    self.postroutes = { };
+
+    self.postroutes['/upload'] = function(req, res) {
+        var form = new formidable.IncomingForm();
+        var fieldValues={};
+        form.on('field', function(field, value) {
+          console.log(field, value);
+          fieldValues[field]=value;
+        })
+        .on('end', function(fields, files) {
+            var temp_path = this.openedFiles[0].path;
+            var file_name = this.openedFiles[0].name;
+
+            fse.copy(temp_path, self.imagedir + file_name, function(err) {
+                if (err) {
+                  console.error(err);
+                } else {
+                  quickthumb.convert({
+                    src: self.imagedir + file_name,
+                    dst: self.imagedir + "thumbs/" + file_name,
+                    height: 200
+                  }, function (err, path) {
+                    if (err) {
+                      console.error(err);
+                    }
+                  });
+                }
+            });
+
+
+            self.db.collection('Paintings').insert(fieldValues, function(err, result) {
+              if(err) { throw err; }
+              res.write("<p>Product inserted:</p>");
+              res.end("<p>" + result[0].title + " " + result[0].price + "</p>");
+            });
+        });
+
+
+        form.parse(req, function(err, fields, files) {
+                          res.writeHead(200, {'content-type': 'text/plain'});
+                          res.write('received upload:\n\n');
+                          res.end(util.inspect({fields: fields, files: files}));
+                        });
+    };
+};
