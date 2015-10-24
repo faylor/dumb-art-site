5eb9f1f878fa50363b5cd9b82965f110ea6f6fb2
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-10-24 12:22:05 +0100

    upload adding to gallery

---
+++ node_config/controllers/users.js
@@ -0,0 +1,38 @@
+
+/**
+ * Module dependencies.
+ */
+
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+
+
+
+
+exports.login = function (req, res) {
+    var username = req.body.username || '';
+    var password = req.body.password || '';
+
+    if (username == '' || password == '') {
+        return res.send(401);
+    }
+
+    User.findOne({username: username}, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.send(401);
+        }
+
+        user.comparePassword(password, function(isMatch) {
+            if (!isMatch) {
+                console.log("Attempt failed to login with " + user.username);
+                return res.send(401);
+            }
+
+            var token = jwt.sign(user, secret.secretToken, { expiresInMinutes: 60 });
+
+            return res.json({token:token});
+        });
+
+    });
+};
--- node_config/models/user.js
+++ node_config/models/user.js
@@ -1,186 +1,33 @@
-/**
- * Module dependencies.
- */
-
-var mongoose = require('mongoose');
-var crypto = require('crypto');
-
 var Schema = mongoose.Schema;
-var oAuthTypes = [
-  'github',
-  'twitter',
-  'facebook',
-  'google',
-  'linkedin'
-];
 
-/**
- * User Schema
- */
-
-var UserSchema = new Schema({
-  name: { type: String, default: '' },
-  email: { type: String, default: '' },
-  username: { type: String, default: '' },
-  provider: { type: String, default: '' },
-  hashed_password: { type: String, default: '' },
-  salt: { type: String, default: '' },
-  authToken: { type: String, default: '' },
-  facebook: {},
-  twitter: {},
-  github: {},
-  google: {},
-  linkedin: {}
+// User schema
+var User = new Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true}
 });
 
-/**
- * Virtuals
- */
+// Bcrypt middleware on UserSchema
+User.pre('save', function(next) {
+  var user = this;
 
-UserSchema
-  .virtual('password')
-  .set(function(password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
-  })
-  .get(function() { return this._password });
+  if (!user.isModified('password')) return next();
 
-/**
- * Validations
- */
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
 
-var validatePresenceOf = function (value) {
-  return value && value.length;
-};
-
-// the below 5 validations only apply if you are signing up traditionally
-
-UserSchema.path('name').validate(function (name) {
-  if (this.skipValidation()) return true;
-  return name.length;
-}, 'Name cannot be blank');
-
-UserSchema.path('email').validate(function (email) {
-  if (this.skipValidation()) return true;
-  return email.length;
-}, 'Email cannot be blank');
-
-UserSchema.path('email').validate(function (email, fn) {
-  var User = mongoose.model('User');
-  if (this.skipValidation()) fn(true);
-
-  // Check only when it is a new user or when email field is modified
-  if (this.isNew || this.isModified('email')) {
-    User.find({ email: email }).exec(function (err, users) {
-      fn(!err && users.length === 0);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
     });
-  } else fn(true);
-}, 'Email already exists');
+  });
+});
 
-UserSchema.path('username').validate(function (username) {
-  if (this.skipValidation()) return true;
-  return username.length;
-}, 'Username cannot be blank');
-
-UserSchema.path('hashed_password').validate(function (hashed_password) {
-  if (this.skipValidation()) return true;
-  return hashed_password.length && this._password.length;
-}, 'Password cannot be blank');
-
-
-/**
- * Pre-save hook
- */
-
-UserSchema.pre('save', function(next) {
-  if (!this.isNew) return next();
-
-  if (!validatePresenceOf(this.password) && !this.skipValidation()) {
-    next(new Error('Invalid password'));
-  } else {
-    next();
-  }
-})
-
-/**
- * Methods
- */
-
-UserSchema.methods = {
-
-  /**
-   * Authenticate - check if the passwords are the same
-   *
-   * @param {String} plainText
-   * @return {Boolean}
-   * @api public
-   */
-
-  authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password;
-  },
-
-  /**
-   * Make salt
-   *
-   * @return {String}
-   * @api public
-   */
-
-  makeSalt: function () {
-    return Math.round((new Date().valueOf() * Math.random())) + '';
-  },
-
-  /**
-   * Encrypt password
-   *
-   * @param {String} password
-   * @return {String}
-   * @api public
-   */
-
-  encryptPassword: function (password) {
-    if (!password) return '';
-    try {
-      return crypto
-        .createHmac('sha1', this.salt)
-        .update(password)
-        .digest('hex');
-    } catch (err) {
-      return '';
-    }
-  },
-
-  /**
-   * Validation is not required if using OAuth
-   */
-
-  skipValidation: function() {
-    return ~oAuthTypes.indexOf(this.provider);
-  }
+//Password verification
+User.methods.comparePassword = function(password, cb) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(isMatch);
+    });
 };
-
-/**
- * Statics
- */
-
-UserSchema.statics = {
-
-  /**
-   * Load
-   *
-   * @param {Object} options
-   * @param {Function} cb
-   * @api private
-   */
-
-  load: function (options, cb) {
-    options.select = options.select || 'name username';
-    this.findOne(options.criteria)
-      .select(options.select)
-      .exec(cb);
-  }
-}
-
 mongoose.model('User', UserSchema);
