fb18904032cd7019e8f45ee046407ba1e67c85b3
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-12-11 17:31:44 +0000

    updated all pages to be dynamic

--- components/templates/standard.tpl.js
+++ components/templates/standard.tpl.js
@@ -1,5 +1,5 @@
-app.controller('standardTemplateController', ['$scope','$rootScope','$http','$routeParams','_','pageFactory',
-    function ( $scope, $rootScope, $http, $routeParams, _, pageFactory){
+app.controller('standardTemplateController', ['$scope','$rootScope','$http','$routeParams','_','pageFactory','paintingFactory',
+    function ( $scope, $rootScope, $http, $routeParams, _, pageFactory, paintingFactory){
 
       $scope.heading = '';
       $scope.subheading = '';
@@ -9,12 +9,18 @@ app.controller('standardTemplateController', ['$scope','$rootScope','$http','$ro
 
       if($rootScope.pages.length > 0){
           $scope.page = _.findWhere($rootScope.pages, {menulink: $routeParams.pagelink});
-
+          $scope.image = getImageFromId($scope.page.image);
           if($scope.page == null){
             $scope.errormessage = "Unable to load page.";
           }
       }else{
         $scope.errormessage = "Unable to load pages.";
       }
+
+       function getImageFromId(id) {
+         paintingFactory.getPainting(id).then(function(data){
+           return data.image;
+         });
+       }
 
 }]);
