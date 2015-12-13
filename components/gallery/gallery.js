a553ce4e135ff7404080937d06c895144a03b63e
System Administrator <root@James-Taylors-MacBook-Air.local>
2015-12-13 20:45:31 +0000

    updated all pages to be dynamic

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -83,7 +83,7 @@ app.controller('galleryController', ['$scope', '$http','$q','$timeout','$documen
       deferred.reject();
     };
 
-    image.src = $scope.images[i].image;
+    image.src = 'images/'+$scope.images[i].image;
     $scope.loading = true;
 
     return deferred.promise;
