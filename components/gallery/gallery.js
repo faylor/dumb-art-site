02ba966c70011e6913b82e380cdddd04bd689079
System Administrator <root@James-Taylors-MacBook-Air.local>
2016-03-01 23:45:50 +0000

    Added Themes

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -1,204 +1,249 @@
-app.controller('galleryController', ['$scope','$rootScope', '$http','$timeout','$document','$uibModal', 'paintingFactory',
-  function($scope, $rootScope, $http, $timeout, $document,$uibModal, paintingFactory) {
-  $scope.name = 'Galleries';
-  $scope.images = [];
-  $scope.totalItems;
-  $scope.currentPage = 1;
-  $scope.maxSize = 5;
-  $scope.thumbsNum = 6;
-  $scope.totalDisplayed = 12;
-  $scope.filterSold = 0;
-  getPaintings();
+app.controller('galleryController', ['$scope', '$rootScope', '$http', '$timeout', '$document', '$uibModal', 'paintingFactory', 'themeFactory',
+  function($scope, $rootScope, $http, $timeout, $document, $uibModal, paintingFactory, themeFactory) {
+    $scope.name = 'Galleries';
+    $scope.images = [];
+    $scope.totalItems;
+    $scope.currentPage = 1;
+    $scope.maxSize = 5;
+    $scope.thumbsNum = 6;
+    $scope.totalDisplayed = 12;
+    $scope.soldFilter = null;
+    $scope.themeFilter = null;
+    getPaintings();
+    getThemes();
 
-  function getPaintings() {
-    paintingFactory.getPaintings()
-      .success(function(p) {
-        $scope.images = p;
-        $scope.imagesLimited = p.slice(0,$scope.totalDisplayed );
-        $scope.totalItems = p.length;
-        //calculateLandscapes();
-      })
-      .error(function(error) {
-        $scope.status = 'Unable to load Painting data: ' + error.message;
-      });
-  }
-
-  $scope.refresh = function(){
-    angularGridInstance.gallery.refresh();
-  }
-
-  $scope.isForSale = function(p) {
-    return ($scope.filterSold === 1 && p.sold === 0) || $scope.filterSold === 0;
-  };
-
-  $scope.loadMore = function() {
-    if($scope.totalDisplayed<$scope.images.length){
-      $scope.totalDisplayed = $scope.totalDisplayed + $scope.thumbsNum;
-      $scope.imagesLimited = $scope.images.slice(0,$scope.totalDisplayed);
+    function getPaintings() {
+      paintingFactory.getPaintings()
+        .success(function(p) {
+          $scope.images = p;
+          $scope.imagesLimited = p.slice(0, $scope.totalDisplayed);
+          $scope.totalItems = p.length;
+          //calculateLandscapes();
+        })
+        .error(function(error) {
+          $scope.status = 'Unable to load Painting data: ' + error.message;
+        });
     }
-  };
 
-  var defaults = {
-		baseClass   : 'ng-gallery',
-		thumbClass  : 'ng-thumb'
-	};
+    function getThemes() {
+      themeFactory.getThemes()
+        .success(function(t) {
+          $scope.themes = t;
+        })
+        .error(function(error) {
+          $scope.status = 'Unable to load Themes data: ' + error.message;
+        });
+    }
 
-	var keys_codes = {
-		enter : 13,
-		esc   : 27,
-		left  : 37,
-		right : 39
-	};
-
-  function setScopeValues(scope) {
-    scope.baseClass = scope.class || defaults.baseClass;
-    scope.thumbClass = scope.thumbClass || defaults.thumbClass;
-    scope.thumbsNum = scope.thumbsNum || 3; // should be odd
-  }
-  setScopeValues($scope);
-
-  if ($scope.thumbsNum >= 11) {
-    $scope.thumbsNum = 2;
-  }
-
-  var $body = $document.find('body');
-  var $thumbwrapper = angular.element(document.querySelectorAll('.ng-thumbnails-wrapper'));
-
-
-  $scope.index = 0;
-  $scope.opened = false;
-
-  $scope.thumb_wrapper_width = 0;
-  $scope.thumbs_width = 0;
-
-
-  var $lastEnlarged = null;
-
-  $scope.togglePic = function(p) {
-    if(p.enlarged == 'true'){
-      p.enlarged = ''
-    }else{
-      p.enlarged = 'true';
-      if($lastEnlarged){
-        $lastEnlarged.enlarged = '';
-      }
-      $lastEnlarged = p;
+    $scope.onChangeSetID = function(id) {
+      $scope.themeFilter = id;
+      $scope.$applyAsync();
     };
-    $rootScope.$broadcast('masonry.reload');
-  };
-
-  $scope.openGallery = function(i) {
-    if (typeof i !== undefined) {
-      $scope.index = i;
-      showImage($scope.index);
-    }
-    $scope.opened = true;
-  };
-
-  $scope.closeGallery = function() {
-    $scope.opened = false;
-  };
-
-
-  $scope.open = function (p) {
-
-    var modalInstance = $uibModal.open({
-      animation: $scope.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: 'lg',
-      resolve: {
-        painting: function () {
-          return p;
+    $scope.clearFilters = function() {
+      $scope.soldFilter = null;
+      $scope.themeFilter = null;
+    };
+    $scope.setFilterSold = function() {
+      $scope.soldFilter = true;
+    };
+    $scope.setFilterUnSold = function() {
+      $scope.soldFilter = false;
+    };
+    $scope.filterSold = function(painting) {
+      return $scope.soldFilter==null || painting.sold==$scope.soldFilter;
+    };
+    $scope.filterFn = function(painting) {
+      if ($scope.themeFilter!=null) {
+        var a = _.find(painting.themes, function(t) {
+          return t._id == $scope.themeFilter
+        });
+        if (a) {
+          return true; // this will be listed in the results
+        }else{
+          return false;
         }
       }
-    });
+      console.log('true');
+      return true;
+    };
 
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      //$log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-
-  $body.bind('keydown', function(event) {
-    if (!$scope.opened) {
-      return;
-    }
-    var which = event.which;
-    if (which === keys_codes.esc) {
-      $scope.closeGallery();
-    } else if (which === keys_codes.right || which === keys_codes.enter) {
-      $scope.nextImage();
-    } else if (which === keys_codes.left) {
-      $scope.prevImage();
+    $scope.refresh = function() {
+      angularGridInstance.gallery.refresh();
     }
 
-    $scope.$apply();
-  });
+    $scope.isForSale = function(p) {
+      return ($scope.filterSold === 1 && p.sold === 0) || $scope.filterSold === 0;
+    };
 
-  var calculateLandscapes = function() {
-    var width = 0,
-    visible_width = 0;
-    $timeout(function(){
-      var $thumbnails = angular.element(document.querySelectorAll('.landscapeCheck'));
-      angular.forEach($thumbnails, function(thumb) {
-        if(thumb.clientWidth>(1.2*thumb.clientHeight)) angular.element(thumb).parent().removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-3').addClass('col-xs-12 col-sm-12 col-md-8 col-lg-6');
-      });
-    },0);
-
-
-
-  };
-
-  var smartScroll = function(index) {
-    $timeout(function() {
-      var len = $scope.images.length,
-        width = $scope.thumbs_width,
-        current_scroll = $thumbwrapper[0].scrollLeft,
-        item_scroll = parseInt(width / len, 10),
-        i = index + 1,
-        s = Math.ceil(len / i);
-
-      $thumbwrapper[0].scrollLeft = 0;
-      $thumbwrapper[0].scrollLeft = i * item_scroll - (s * item_scroll);
-    }, 100);
-  };
-
-}]);
-app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', '$q','painting',
-function ($scope, $uibModalInstance, $q, painting) {
-
-
-  $scope.ok = function () {
-    $uibModalInstance.close(1);
-  };
-
-  $scope.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-
-  var loadImage = function(i) {
-    var deferred = $q.defer();
-    var image = new Image();
-
-    image.onload = function() {
-      $scope.loading = false;
-      if (typeof this.complete === false || this.naturalWidth === 0) {
-        deferred.reject();
+    $scope.loadMore = function() {
+      if ($scope.totalDisplayed < $scope.images.length) {
+        $scope.totalDisplayed = $scope.totalDisplayed + $scope.thumbsNum;
+        $scope.imagesLimited = $scope.images.slice(0, $scope.totalDisplayed);
       }
-      deferred.resolve(image);
     };
 
-    image.onerror = function() {
-      deferred.reject();
+    var defaults = {
+      baseClass: 'ng-gallery',
+      thumbClass: 'ng-thumb'
     };
 
-    image.src = 'images/'+i;
-    $scope.loading = true;
+    var keys_codes = {
+      enter: 13,
+      esc: 27,
+      left: 37,
+      right: 39
+    };
 
-    return deferred.promise;
-  };
+    function setScopeValues(scope) {
+      scope.baseClass = scope.class || defaults.baseClass;
+      scope.thumbClass = scope.thumbClass || defaults.thumbClass;
+      scope.thumbsNum = scope.thumbsNum || 3; // should be odd
+    }
+    setScopeValues($scope);
+
+    if ($scope.thumbsNum >= 11) {
+      $scope.thumbsNum = 2;
+    }
+
+    var $body = $document.find('body');
+    var $thumbwrapper = angular.element(document.querySelectorAll('.ng-thumbnails-wrapper'));
+
+
+    $scope.index = 0;
+    $scope.opened = false;
+
+    $scope.thumb_wrapper_width = 0;
+    $scope.thumbs_width = 0;
+
+
+    var $lastEnlarged = null;
+
+    $scope.togglePic = function(p) {
+      if (p.enlarged == 'true') {
+        p.enlarged = ''
+      } else {
+        p.enlarged = 'true';
+        if ($lastEnlarged) {
+          $lastEnlarged.enlarged = '';
+        }
+        $lastEnlarged = p;
+      };
+      $rootScope.$broadcast('masonry.reload');
+    };
+
+    $scope.openGallery = function(i) {
+      if (typeof i !== undefined) {
+        $scope.index = i;
+        showImage($scope.index);
+      }
+      $scope.opened = true;
+    };
+
+    $scope.closeGallery = function() {
+      $scope.opened = false;
+    };
+
+
+    $scope.open = function(p) {
+
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        size: 'lg',
+        resolve: {
+          painting: function() {
+            return p;
+          }
+        }
+      });
+
+      modalInstance.result.then(function(selectedItem) {
+        $scope.selected = selectedItem;
+      }, function() {
+        //$log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    $body.bind('keydown', function(event) {
+      if (!$scope.opened) {
+        return;
+      }
+      var which = event.which;
+      if (which === keys_codes.esc) {
+        $scope.closeGallery();
+      } else if (which === keys_codes.right || which === keys_codes.enter) {
+        $scope.nextImage();
+      } else if (which === keys_codes.left) {
+        $scope.prevImage();
+      }
+
+      $scope.$apply();
+    });
+
+    var calculateLandscapes = function() {
+      var width = 0,
+        visible_width = 0;
+      $timeout(function() {
+        var $thumbnails = angular.element(document.querySelectorAll('.landscapeCheck'));
+        angular.forEach($thumbnails, function(thumb) {
+          if (thumb.clientWidth > (1.2 * thumb.clientHeight)) angular.element(thumb).parent().removeClass('col-xs-12 col-sm-6 col-md-4 col-lg-3').addClass('col-xs-12 col-sm-12 col-md-8 col-lg-6');
+        });
+      }, 0);
+
+
+
+    };
+
+    var smartScroll = function(index) {
+      $timeout(function() {
+        var len = $scope.images.length,
+          width = $scope.thumbs_width,
+          current_scroll = $thumbwrapper[0].scrollLeft,
+          item_scroll = parseInt(width / len, 10),
+          i = index + 1,
+          s = Math.ceil(len / i);
+
+        $thumbwrapper[0].scrollLeft = 0;
+        $thumbwrapper[0].scrollLeft = i * item_scroll - (s * item_scroll);
+      }, 100);
+    };
+
+  }
+]);
+app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', '$q', 'painting',
+  function($scope, $uibModalInstance, $q, painting) {
+
+
+    $scope.ok = function() {
+      $uibModalInstance.close(1);
+    };
+
+    $scope.cancel = function() {
+      $uibModalInstance.dismiss('cancel');
+    };
+
+    var loadImage = function(i) {
+      var deferred = $q.defer();
+      var image = new Image();
+
+      image.onload = function() {
+        $scope.loading = false;
+        if (typeof this.complete === false || this.naturalWidth === 0) {
+          deferred.reject();
+        }
+        deferred.resolve(image);
+      };
+
+      image.onerror = function() {
+        deferred.reject();
+      };
+
+      image.src = 'images/' + i;
+      $scope.loading = true;
+
+      return deferred.promise;
+    };
 
     var showImage = function(painting) {
       loadImage(painting.image).then(function(resp) {
@@ -216,7 +261,7 @@ function ($scope, $uibModalInstance, $q, painting) {
       $scope.index = i;
       loadImage($scope.index.image).then(function(resp) {
         $scope.img = resp.src;
-      //  smartScroll($scope.index.);
+        //  smartScroll($scope.index.);
       });
       $scope.title = $scope.index.title || '';
       $scope.price = $scope.index.price || '';
@@ -243,4 +288,5 @@ function ($scope, $uibModalInstance, $q, painting) {
 
     showImage(painting);
 
-}]);
+  }
+]);
