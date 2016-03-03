53e9341e7452da48cde90d783ab64222dcbd208d
System Administrator <root@James-Taylors-MacBook-Air.local>
2016-03-03 23:39:45 +0000

    Added Themes

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -51,7 +51,7 @@ app.controller('galleryController', ['$scope', '$rootScope', '$http', '$timeout'
       $scope.soldFilterName = null;
       $scope.themeFilter = null;
       $scope.themeFilterName = null;
-      $scope.imagesLimited = 12;
+      $scope.totalDisplayed = 12;
       $scope.loadMore();
       $scope.$applyAsync();
     };
