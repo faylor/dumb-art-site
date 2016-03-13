3314019ea14087c84e813f2e89d4fb0c110fcf4e
System Administrator <root@James-Taylors-MacBook-Air.local>
2016-03-13 10:44:44 +0000

    angulartics added

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
-var app = angular.module('galleryApp', ['ngRoute','ui.bootstrap','ngSanitize','wu.masonry','infinite-scroll','galleryApp.dragdrop','angulartics', 'angulartics.google.analytics'])
+
+require('angulartics');
+var app = angular.module('galleryApp', ['ngRoute','ui.bootstrap','ngSanitize','wu.masonry','infinite-scroll','galleryApp.dragdrop','angulartics', require('angulartics-google-analytics')])
 
 app.config(['$locationProvider','$routeProvider',
   function($locationProvider,$routeProvider) {
