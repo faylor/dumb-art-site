84b5ba789a411bbfad3fb9e661aa632c2965f8e0
System Administrator <root@James-Taylors-MacBook-Air.local>
2017-09-30 22:33:27 +0100

    V3 Upgrade

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ var SampleApp = function() {
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 0.0.0.0');
+            self.ipaddress = "0.0.0.0";
         };
         //var dbName = "/nodejs";
         //var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + dbName;
