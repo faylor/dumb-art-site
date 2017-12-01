baba54d4936e678b9a2ef3887cca7073498d4025
James Taylor <jamestaylor@James-Taylors-MacBook-Air.local>
2017-12-01 22:37:56 +0000

    heroku changes - versions node

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var SampleApp = function() {
         //var dbName = "/nodejs";
         //var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + dbName;
         //self.db = mongojs(connection_string, ['Paintings']);
-        self.url = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
+        self.url = process.env.DATABASE_URL;
         if(process.env.NODE_ENV=="dev") self.url = 'mongodb://127.0.0.1:27017/nodejs';
     };
 
