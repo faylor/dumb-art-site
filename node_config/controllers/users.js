41f96bc2aa3c05083397fb256649cb545838cee7
faylor <32422243+faylor@users.noreply.github.com>
2021-03-29 20:53:25 +0100

    Make dummy change

--- node_config/controllers/users.js
+++ node_config/controllers/users.js
@@ -42,52 +42,9 @@ exports.login = function (req, res) {
         }
 
       });
-      /*User.findOne({username:fieldValues.username}).lean().exec(function(err, user) {
-        if (err) {
-          console.log(err);
-          return res.send(401);
-        }
-        if (user) {
-          if(user.authenticate(password)){
-
-          //if(user.password==fieldValues.password){
-            var token = jwt.sign(user, config.secret, { expiresIn: 3600 });
-
-            return res.json({token:token});
-          }
-        }
-
-      });*/
     });
     form.parse(req, function(err, fields, files) {});
 
-/*
-    User.findOne({username: fieldValues.username}, function (err, obj) {
-        if (err) {
-            console.log(err);
-            return res.send(401);
-        }
-        var user = obj.toObject();
-        console.log("username>>"+user.username);
-        console.log("username>>"+user.password);
-        if(user.password==fieldValues.password){
-          var token = jwt.sign(user, secret.secretToken, { expiresInMinutes: 60 });
-
-          return res.json({token:token});
-        }
-        user.comparePassword(fieldValues.password, function(isMatch) {
-            if (!isMatch) {
-                console.log("Attempt failed to login with " + user.username);
-                return res.send(401);
-            }
-
-            var token = jwt.sign(user, secret.secretToken, { expiresInMinutes: 60 });
-
-            return res.json({token:token});
-        });
-
-    });
-    */
 };
 
 
